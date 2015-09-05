fd93cb782909e70a1b3dd906a5895e0f87862873
Timothy Marois <timothymarois@gmail.com>
2015-09-05 14:58:55 -0400

    Returning values from INSERT/UPDATE/DELETE
    UPDATE and DELETE both will return the value of
    ->affected_rows
    
    INSERT will return the value of
    ->insert_id

--- database/mysqli/extend_mysqli.php
+++ database/mysqli/extend_mysqli.php
@@ -1,17 +1,30 @@
 <?php 
+
  /**
   * Extends mysqli and adds the ability to easily apc cache queries
   */
 class extend_mysqli extends mysqli 
 {
 	/**
-     * This Function overwrites the mysql query function but should return the same objects
-     */
+   * This Function overwrites the mysql query function but should return the same objects
+   */
 	public function query($query,$resultmode=null)
 	{
-		if($return = parent::query($query))
+		if($return = parent::query($query)) 
 		{
-			if ($resultmode==='row') 
+			if (preg_match('/^\s*DELETE\s+FROM\s*/i',$query)) {
+				return $this->affected_rows;
+			}
+
+			if (preg_match('/^\s*UPDATE(.*)SET/i',$query)) {
+				return $this->affected_rows;
+			}
+
+			if (preg_match('/^\s*INSERT\s+INTO/i',$query)) {
+				return $this->insert_id;
+			}
+
+			/*if ($resultmode==='row') 
 			{
 				$r = $return->fetch_object();
 				$return->free();
@@ -23,9 +36,9 @@ class extend_mysqli extends mysqli
 			}
 			else {
 				$r = true;
-			}
+			}*/
 
-			return $r;
+			// return $this->affected_rows;
 		}
 		else
 		{
@@ -35,8 +48,8 @@ class extend_mysqli extends mysqli
 
 	
 	/**
-     * This function loops through the results and returns them as an array of objects
-     */
+   * This function loops through the results and returns them as an array of objects
+   */
 	private function _fetch_assoc_obj_array(mysqli_result $result)
 	{
 		$array = array();
@@ -51,8 +64,8 @@ class extend_mysqli extends mysqli
     
 	
 	/**
-     * This function loops through the results and returns them as an array of objects
-     */
+   * This function loops through the results and returns them as an array of objects
+   */
 	private function _buildArray(mysqli_result $result)
 	{
 		$array = array();
@@ -64,6 +77,6 @@ class extend_mysqli extends mysqli
     	
 		return $array;
 	}
-	 
  }
  
+
